323e9432179726745d0257648aa9a3c287664381
Rubén Ramírez Gómez <maths.unam@gmail.com>
2021-04-17 15:07:58 -0500

    First Commit

---
+++ plugins/imgx-sitios/imgx-sitios.php
@@ -0,0 +1,13 @@
+<?php
+
+    /**
+     * Plugin Name: Sitios para visitar
+     * Plugin URI: imagilex.com.mx
+     * Description: Plugin para posts tipo "sitio para visitar"
+     * Version: 1.0
+     * Author: Ruben Ramirez Gomez
+     * Author URI: rramirez.com
+     * License: GPL
+     */
+
+?>
